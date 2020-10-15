bb32d7ab2078d9cb8de6e0fa0dc5a03c83fa2760
Grzegorz Piechnik <grzegorzpiechnik@MacBook-Pro-Grzegorz.local>
2020-10-15 11:08:09 +0200

    Removed 'username' from User model

--- popita/accounts/models.py
+++ popita/accounts/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
 class User(AbstractUser):
+    username = None
+    username_validator = None
     email = models.EmailField(verbose_name = 'email', max_length = 255, unique = True)
-    REQUIRED_FIELDS = ['first_name']
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
+
+    objects = CustomUserManager()
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_UNIQUE_EMAIL = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = 'email'
+    ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+    ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+    ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = '/?verification=1'
+    ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/?verification=1'
+
 
     def get_username(self):
         return self.email
--- popita/popita/settings.py
+++ popita/popita/settings.py
@@ -134,6 +134,10 @@ DJOSER = {
     },
 }
 
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'users.serializers.UserSerializer',
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
