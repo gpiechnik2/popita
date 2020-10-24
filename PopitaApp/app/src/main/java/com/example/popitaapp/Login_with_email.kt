11d243f9a19ff4fd7bbe7fac4e4cdf304fe31481
Grzegorz Piechnik <grzegorzpiechnik@MacBook-Pro-Grzegorz.local>
2020-10-24 23:49:37 +0200

    Added login with email layout

---
+++ PopitaApp/app/src/main/java/com/example/popitaapp/Login_with_email.kt
@@ -0,0 +1,11 @@
+package com.example.popitaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_with_email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_with_email)
+    }
+}
