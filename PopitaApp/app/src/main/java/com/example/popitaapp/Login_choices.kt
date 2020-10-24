e8d16c907bed87f98546ecd285e89a170addc0a3
Grzegorz Piechnik <grzegorzpiechnik@MacBook-Pro-Grzegorz.local>
2020-10-24 06:35:59 +0200

    Added Login choose Activity

--- PopitaApp/app/src/main/java/com/example/popitaapp/Login_choices.kt
+++ PopitaApp/app/src/main/java/com/example/popitaapp/Login_choices.kt
@@ -2,14 +2,10 @@ package com.example.popitaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
 
-
-class MainActivity : AppCompatActivity() {
+class Login_choices : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        }
+        setContentView(R.layout.activity_login_choices)
     }
 }
